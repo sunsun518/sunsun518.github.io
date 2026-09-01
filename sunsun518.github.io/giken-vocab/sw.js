304cb44024e1f9f6d9545c2a92c6ac72388a201d
sunsun518 <ishish0518@gmail.com>
2026-09-01 22:19:15 +0900

    Add files via upload

---
+++ sunsun518.github.io/giken-vocab/sw.js
@@ -0,0 +1,31 @@
+const CACHE = "giken-vocab-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// キャッシュ優先、なければネットワーク（オフラインでも学習を継続できるように）
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
